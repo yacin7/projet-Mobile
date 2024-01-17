c2709bbf955caad1156b59d8a118eaaf6c3c11b9
malekayedi <80425263+malekayedi@users.noreply.github.com>
2024-01-17 13:32:45 +0100

    first commit

--- app/src/main/java/com/example/gestion_produit/front/Constants.java
+++ app/src/main/java/com/example/gestion_produit/front/Constants.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Constants {
-    public static final String BASE_URL="http://192.168.1.15:5000/";
+    public static final String BASE_URL="http://192.168.39.62:5000/";
 
 
 }
